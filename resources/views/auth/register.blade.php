4fb9f8dc8afee1e4f0fd587054f5d61558f19912
Jorge <jorferr89@gmail.com>
2024-11-28 22:02:03 -0300

    creacion de login, register e incorporacion de bootstrap

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,69 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container bg-light text-dark rounded p-2 col-12 col-sm-10 col-lg-6 mx-auto">
+    <h3>Registrarme</h3>
+    <form method="POST" action="{{ route('register') }}">
+        @csrf
+
+        <div class="row mb-3">
+            <label for="name" class="col-md-4 col-form-label text-md-end">Nombre</label>
+
+            <div class="col-md-6">
+                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                @error('name')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+        </div>
+
+        <div class="row mb-3">
+            <label for="email" class="col-md-4 col-form-label text-md-end">Correo Electrónico</label>
+
+            <div class="col-md-6">
+                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+        </div>
+
+        <div class="row mb-3">
+            <label for="password" class="col-md-4 col-form-label text-md-end">Contraseña</label>
+
+            <div class="col-md-6">
+                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+        </div>
+
+        <div class="row mb-3">
+            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">Confirmar Contraseña</label>
+
+            <div class="col-md-6">
+                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+            </div>
+        </div>
+
+        <div class="d-grid gap-2">
+            <button type="submit" class="btn btn-primary">
+                Registrarme
+            </button>
+        </div>
+    </form>
+
+</div>
+
+@endsection
